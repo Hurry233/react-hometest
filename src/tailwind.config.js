22d3e0312d4947817d6090e278df8bd1bbfda349
Hurry233 <80822989+Hurry233@users.noreply.github.com>
2025-10-23 21:34:03 +0800

    Add files via upload

---
+++ src/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './src/components/**/*.tsx',
+    './src/components/**/*.jsx',
+    './src/components/**/*.md',
+    './src/components/**/*.json',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
